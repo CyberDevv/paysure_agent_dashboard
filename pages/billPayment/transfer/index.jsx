455347075fb1b2488f1894d04e15496b91d3ccd5
Cyberdevv <tocyberdevv@gmail.com>
2022-03-24 16:08:58 +0100

    💥 NEW: develop the transfer page

---
+++ pages/billPayment/transfer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import tw from 'twin.macro'
+import Head from 'next/head'
+
+import { TransferDashboard } from '../../../component'
+
+const TransferPage = () => (
+  <div>
+    <Head>
+      <title>Transfer | PaySure</title>
+    </Head>
+
+    <TransferDashboard />
+  </div>
+)
+
+export default TransferPage
