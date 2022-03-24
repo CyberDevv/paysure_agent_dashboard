f3cebf8039100de8bdcea87007d8a6fc4ef7fede
Cyberdevv <tocyberdevv@gmail.com>
2022-03-24 16:15:41 +0100

    💥 NEW: develop the internet page

---
+++ pages/billPayment/internet/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import tw from 'twin.macro'
+import Head from 'next/head'
+
+import { InternetDashboard } from '../../../component'
+
+const InternetPage = () => (
+  <div>
+    <Head>
+      <title>Internet | PaySure</title>
+    </Head>
+
+    <InternetDashboard />
+  </div>
+)
+
+export default InternetPage
